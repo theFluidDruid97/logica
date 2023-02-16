3d6f933491dbb8087572a6323e73fb076506b455
SeaWolf2285 <110475779+SeaWolf2285@users.noreply.github.com>
2023-02-16 10:54:59 -0600

    updated notifictions
    changed to a toaster

--- web/src/components/Notification/Notification.js
+++ web/src/components/Notification/Notification.js
@@ -1,35 +1,20 @@
-const Notify = (title, body) => {
-  let permission = Notification.permission
+import { toast } from '@redwoodjs/web/dist/toast'
 
-  if (permission === 'granted') {
-    showNotification()
-  } else if (permission === 'default') {
-    requestAndShowPermission()
-  } else {
-    alert('Use normal alert')
-  }
+import { GeneralContext } from 'src/App'
+// import { Success } from '../Airman/AirmanCell/AirmanCell'
 
-  function requestAndShowPermission() {
-    Notification.requestPermission(function (permission) {
-      if (permission === 'granted') {
-        showNotification()
-      }
-    })
-  }
-  function showNotification() {
-    //  if(document.visibilityState === "visible") {
-    //      return;
-    //  }
-    // let title = 'You have Training thats due soon!'
-    // let body = 'Get it done or youll be in trouble!'
-
-    let notification = new Notification(title, { body })
-
-    notification.onclick = () => {
-      notification.close()
-      window.parent.focus()
-    }
+export const Notify = (currentUser) => {
+  const { notified, setNotified } = React.useContext(GeneralContext)
+  if (currentUser.status === 'Due' && notified === false) {
+    toast('You have Training that is due! Please check your training list.')
+    setNotified(true)
+  } else if (currentUser.status === 'Overdue' && notified === false) {
+    toast(
+      'You have Training that is overdue! Complete your training immediately.'
+    )
+    setNotified(true)
+  } else if (currentUser.status === 'Current' && notified === false) {
+    toast('You have no training due or overdue.')
+    setNotified(true)
   }
 }
-
-export default Notify
