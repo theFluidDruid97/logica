cb082025c808bd62295d65e0b934ac8d8adb49f9
theFluidDruid97 <artisan.graph@gmail.com>
2023-02-17 11:10:14 -0600

    reset stability

--- web/src/pages/NotificationList/EditNotificationListPage/EditNotificationListPage.js
+++
@@ -1,7 +0,0 @@
-import EditNotificationListCell from 'src/components/NotificationList/EditNotificationListCell'
-
-const EditNotificationListPage = ({ id }) => {
-  return <EditNotificationListCell id={id} />
-}
-
-export default EditNotificationListPage
--- web/src/pages/NotificationList/NotificationListsPage/NotificationListsPage.js
+++
@@ -1,7 +0,0 @@
-import NotificationListsCell from 'src/components/NotificationList/NotificationListsCell'
-
-const NotificationListsPage = () => {
-  return <NotificationListsCell />
-}
-
-export default NotificationListsPage
