ea74e1d3449ad3fd93a0e413f3189cbe9ab4c26e
Miked88 <mustangmichaeldavis@gmail.com>
2023-02-27 15:28:24 -0600

    training_collections_seed

--- scripts/seed.js
+++ scripts/seed.js
@@ -4,13 +4,16 @@ import { hashPassword } from '@redwoodjs/api'
 
 import { airmen } from './airmen.js'
 import { collections } from './collections.js'
+import { monitors } from './monitors.js'
 import { trainings } from './trainings.js'
 import { airmanTrainings } from './trainings.js'
+import { trainingCollections } from './TrainingCollection.js'
 
 let trainingsCount = 0
 let collectionsCount = 0
 let airmanCount = 0
 let airmanTrainingCount = 0
+let trainingCollectionCount = 0
 const defaultPassword = '123'
 const adminEmail = 'admin@mail.com'
 const adminPassword = '1776'
@@ -28,216 +31,6 @@ const admin = {
   dodId: '1234567890',
   afsc: '6T971',
   roles: 'Admin',
-}
-
-const monitor1 = {
-  email: "dustin.iverson.1@us.af.mil",
-  hashedPassword,
-  salt,
-  rank: 'SSgt',
-  firstName: 'Dustin',
-  middleName: 'William',
-  lastName: 'Iverson',
-  organization: '2 AMXS',
-  officeSymbol: 'A1A',
-  dodId: '2758016437',
-  afsc: '2A551',
-  roles: 'Monitor'
-  }
-
-const monitor2 = {
-  email: "nick.sibus.1@us.af.mil",
-  hashedPassword,
-  salt,
-  rank: 'SSgt',
-  firstName: 'Nicholas',
-  middleName: 'Nick',
-  lastName: 'Sibus',
-  organization: '2 AMXS',
-  officeSymbol: 'A1A',
-  dodId: '8016437104',
-  afsc: '2A551',
-  roles: 'Monitor'
-}
-
-const monitor3 = {
-  email: "john.noah.1@us.af.mil",
-  hashedPassword,
-  salt,
-  rank: 'SSgt',
-  firstName: 'John',
-  middleName: 'Joseph',
-  lastName: 'Noah',
-  organization: '8 FS',
-  officeSymbol: 'F1A',
-  dodId: '4371042341',
-  afsc: '2A951',
-  roles: 'Monitor'
-}
-
-const monitor4 = {
-  email: "cody.carrell.1@us.af.mil",
-  hashedPassword,
-  salt,
-  rank: 'SSgt',
-  firstName: 'Cody',
-  middleName: 'Wade',
-  lastName: 'Carrell',
-  organization: '8 FS',
-  officeSymbol: 'F1A',
-  dodId: '4323418897',
-  afsc: '2A951',
-  roles: 'Monitor'
-}
-
-const monitor5 = {
-  email: "billy.bob.1@us.af.mil",
-  hashedPassword,
-  salt,
-  rank: 'SSgt',
-  firstName: 'BillyBob',
-  middleName: 'Bobby',
-  lastName: 'JimBob',
-  organization: '67 SOS',
-  officeSymbol: 'S1A',
-  dodId: '0987789054',
-  afsc: '3P0X1',
-  roles: 'Monitor'
-}
-
-const monitor6 = {
-  email: "ernest.foster.1@us.af.mil",
-  hashedPassword,
-  salt,
-  rank: 'SSgt',
-  firstName: 'Ernest',
-  middleName: 'NMN',
-  lastName: 'Foster',
-  organization: '67 SOS',
-  officeSymbol: 'S1A',
-  dodId: '0911119054',
-  afsc: '3P0X1',
-  roles: 'Monitor'
-}
-
-const monitor7 = {
-  email: "kayla.smith.1@us.af.mil",
-  hashedPassword,
-  salt,
-  rank: 'SSgt',
-  firstName: 'Kayla',
-  middleName: 'Marie',
-  lastName: 'Smith',
-  organization: '96 BS',
-  officeSymbol: 'S1A',
-  dodId: '2105632793',
-  afsc: '1T0X1',
-  roles: 'Monitor'
-}
-
-const monitor8 = {
-  email: "natalie.davis.1@us.af.mil",
-  hashedPassword,
-  salt,
-  rank: 'SSgt',
-  firstName: 'Natalie',
-  middleName: 'May',
-  lastName: 'Davis',
-  organization: '96 BS',
-  officeSymbol: 'S1A',
-  dodId: '2107220810',
-  afsc: '1T0X1',
-  roles: 'Monitor'
-}
-
-const monitor9 = {
-  email: "emilia.willis.1@us.af.mil",
-  hashedPassword,
-  salt,
-  rank: 'SSgt',
-  firstName: 'Emilia',
-  middleName: 'Kay',
-  lastName: 'Willis',
-  organization: '343 BS',
-  officeSymbol: 'B1A',
-  dodId: '3344550876',
-  afsc: '3P0X1',
-  roles: 'Monitor'
-}
-
-const monitor10 = {
-  email: "goku.son.1@us.af.mil",
-  hashedPassword,
-  salt,
-  rank: 'SSgt',
-  firstName: 'Goku',
-  middleName: 'Kakarot',
-  lastName: 'Son',
-  organization: '343 BS',
-  officeSymbol: 'B1A',
-  dodId: '3256841001',
-  afsc: '3P0X1',
-  roles: 'Monitor'
-}
-
-const monitor11 = {
-  email: "andrew.jackson.1@us.af.mil",
-  hashedPassword,
-  salt,
-  rank: 'SSgt',
-  firstName: 'Andrew',
-  middleName: 'NMN',
-  lastName: 'Jackson',
-  organization: '377 SFS',
-  officeSymbol: 'S1A',
-  dodId: '4329817288',
-  afsc: '4Y0X1',
-  roles: 'Monitor'
-}
-
-const monitor12 = {
-  email: "george.washington.1@us.af.mil",
-  hashedPassword,
-  salt,
-  rank: 'SSgt',
-  firstName: 'George',
-  middleName: 'NMN',
-  lastName: 'Washington',
-  organization: '377 SFS',
-  officeSymbol: 'S1A',
-  dodId: '6608213321',
-  afsc: '4Y0X1',
-  roles: 'Monitor'
-}
-
-const monitor13 = {
-  email: "chris.stephens.1@us.af.mil",
-  hashedPassword,
-  salt,
-  rank: 'SSgt',
-  firstName: 'Chris',
-  middleName: 'Jim',
-  lastName: 'Stephens',
-  organization: '701 MUNS',
-  officeSymbol: 'M1A',
-  dodId: '9871235208',
-  afsc: '5J0X1',
-  roles: 'Monitor'
-}
-
-const monitor14 = {
-  email: "joey.reynolds.1@us.af.mil",
-  hashedPassword,
-  salt,
-  rank: 'SSgt',
-  firstName: 'Joey',
-  middleName: 'Michael',
-  lastName: 'Reynolds',
-  organization: '701 MUNS',
-  officeSymbol: 'M1A',
-  dodId: '8734610001',
-  afsc: '5J0X1',
-  roles: 'Monitor'
 }
 
 const createAirmen = async (airmen) => {
@@ -268,20 +61,31 @@ const createAirmen = async (airmen) => {
 
 const createAirmenTrainings = async (airmanTrainings) => {
   for (let airmanTraining of airmanTrainings) {
-  await db.airmanTraining.create({
-    data: {
-      airmanId: airmanTraining.airmanId,
-      trainingId: airmanTraining.trainingId,
-      status: airmanTraining.status,
-      start: airmanTraining.start,
-      end: airmanTraining.end,
-    },
-  })
-  airmanTrainingCount = airmanTrainingCount + 1
+    await db.airmanTraining.create({
+      data: {
+        airmanId: airmanTraining.airmanId,
+        trainingId: airmanTraining.trainingId,
+        status: airmanTraining.status,
+        start: airmanTraining.start,
+        end: airmanTraining.end,
+      },
+    })
+    airmanTrainingCount = airmanTrainingCount + 1
   }
-  console.log(
-    `\tAIRMAN TRAININGS CREATED\t${airmanTrainingCount}\n\n`
-  )
+  console.log(`\tAIRMAN TRAININGS CREATED\t${airmanTrainingCount}\n`)
+}
+
+const createTrainingCollections = async (trainingCollections) => {
+  for (let trainingCollection of trainingCollections) {
+    await db.trainingCollection.create({
+      data: {
+        trainingId: trainingCollection.trainingId,
+        collectionId: trainingCollection.collectionId,
+      },
+    })
+    trainingCollectionCount = trainingCollectionCount + 1
+  }
+  console.log(`\tTRAININGS ASSIGNED TO COLLECTIONS\t${trainingCollectionCount}\n\n`)
 }
 
 export default async () => {
@@ -289,47 +93,10 @@ export default async () => {
     await db.airman.create({ data: admin })
     airmanCount = airmanCount + 1
 
-    await db.airman.create({ data: monitor1 })
-    airmanCount = airmanCount + 1
-
-    await db.airman.create({ data: monitor2 })
-    airmanCount = airmanCount + 1
-
-    await db.airman.create({ data: monitor3 })
-    airmanCount = airmanCount + 1
-
-    await db.airman.create({ data: monitor4 })
-    airmanCount = airmanCount + 1
-
-    await db.airman.create({ data: monitor5 })
-    airmanCount = airmanCount + 1
-
-    await db.airman.create({ data: monitor6 })
-    airmanCount = airmanCount + 1
-
-    await db.airman.create({ data: monitor7 })
-    airmanCount = airmanCount + 1
-
-    await db.airman.create({ data: monitor8 })
-    airmanCount = airmanCount + 1
-
-    await db.airman.create({ data: monitor9 })
-    airmanCount = airmanCount + 1
-
-    await db.airman.create({ data: monitor10 })
-    airmanCount = airmanCount + 1
-
-    await db.airman.create({ data: monitor11 })
-    airmanCount = airmanCount + 1
-
-    await db.airman.create({ data: monitor12 })
-    airmanCount = airmanCount + 1
-
-    await db.airman.create({ data: monitor13 })
-    airmanCount = airmanCount + 1
-
-    await db.airman.create({ data: monitor14 })
-    airmanCount = airmanCount + 1
+    monitors.map(async (data) => {
+      airmanCount = airmanCount + 1
+      await db.airman.create({ data })
+    })
 
     trainings.map(async (data) => {
       trainingsCount = trainingsCount + 1
@@ -343,6 +110,7 @@ export default async () => {
 
     await createAirmen(airmen)
     await createAirmenTrainings(airmanTrainings)
+    await createTrainingCollections(trainingCollections)
   } catch (error) {
     console.warn('Please define your seed data.')
     console.error(error)
